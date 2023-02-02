88ad005bde6e4d9a0b81f62d0f267cfdc152595c
stephane-evrard <gozzo.ded@student.moringaschool.com>
2023-02-02 17:04:45 +0300

    checking blog url

--- landing_app/forms.py
+++ landing_app/forms.py
@@ -4,6 +4,9 @@ from django.utils.translation import gettext_lazy as _
 from .models import *
 
 
+
+
+# BLOG COMMENT FORM
 class BlogCommentForm(forms.ModelForm):
     class Meta:
         model= BlogComment
@@ -17,4 +20,24 @@ class BlogCommentForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control form--control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'comment': forms.Textarea(attrs={'class': 'form-control', 'row':1, 'cols':30}),
+        }
+
+
+
+
+# CONTACT FORM
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model  = Contact
+        fields = [
+            "first_name",
+            "email",
+            "subject",
+            "message",
+        ]
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email':     forms.EmailInput(attrs={'class': 'form-control'}),
+            'subject':   forms.TextInput(attrs={'class': 'form-control'}),
+            'message':   forms.Textarea(attrs={'class': 'form-control', 'rows':1, 'cols':30}),
         }
