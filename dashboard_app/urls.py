000102dc29aa881d676c14339432a8a7d6b7fb37
Allarassem Maxime <allarassemmaxwell@gmail.com>
2023-02-13 15:22:07 +0100

    Working on a lot of things

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -28,40 +28,57 @@ urlpatterns = [
     # path('testimony/add/', testimony_add_view, name="testimony_add"),
 
 
-    path('service/', service_view, name="service"),
+    
     path('service/category/', service_category_view, name="service_category"),
+    path('service/category/delete/<slug:slug>/', service_category_delete_view, name="service_category_delete"),
+    path('service/category/update/<slug:slug>/', service_category_update_view, name="service_category_update"),
+
+    path('service/', service_view, name="service"),
     path('service/add/', service_add_view, name="service_add"),
     path('service/delete/<slug:slug>/', service_delete_view, name="service_delete"),
-    path('service/category/delete/<slug:slug>/', service_category_delete_view, name="service_category_delete"),
+    path('service/update/<slug:slug>/', service_update_view, name="service_update"),
+    
 
     path('product/', product_view, name="product"),
     path('product/add/', product_add_view, name="product_add"),
-    path('product/image/', product_image_view, name="product_image"),
-    path('product/image/delete/<int:id>/', product_image_delete_view, name="product_image_delete"),
-
     path('product/delete/<slug:slug>/', product_delete_view, name="product_delete"),
+    path('product/update/<slug:slug>/', product_update_view, name="product_update"),
+
+    path('product/image/', product_image_view, name="product_image"),
+    path('product/image/delete/<slug:slug>/', product_image_delete_view, name="product_image_delete"),
+    path('product/image/update/<slug:slug>/', product_image_update_view, name="product_image_update"),
+
+
+
     path('product/category/', product_category_view, name="product_category"),
     path('product/category/delete/<slug:slug>/', product_category_delete_view, name="product_category_delete"),
+    path('product/category/update/<slug:slug>/', product_category_update_view, name="product_category_update"),
 
 
     path('supplier/', supplier_view, name="supplier"),
     path('supplier/add/', supplier_add_view, name="supplier_add"),
     path('supplier/delete/<slug:slug>/', supplier_delete_view, name="supplier_delete"),
+    path('supplier/update/<slug:slug>/', supplier_update_view, name="supplier_update"),
     
     
     path('stock/', stock_view, name="stock"),
     path('stock/add/', stock_add_view, name="stock_add"),
+    path('stock/delete/<str:id>/', stock_delete_view, name="stock_delete"),
+    path('stock/update/<str:id>/', stock_update_view, name="stock_update"),
     
     
-     path('appointment/symptoms/', appointment_symptom_view, name="appointment_symptom"),
-     path('appointment/symptoms/delete/<slug:slug>/', appointment_symptom_delete_view, name="appointment_symptom_delete"),
-     
-     path('appointment/', appointment_view, name="appointment"),
-     path('appointment/add', appointment_add_view, name="appointment_add"),
-     
-     path('appointment/prescription', appointment_prescription_view, name="appointment_prescription"),
-     path('appointment/prescription/add', appointment_prescription_add_view, name="appointment_prescription_add"),
-     path('appointment/prescription/delete/<slug:slug>/', appointment_prescription_delete_view, name="appointment_prescription_delete"),
+    path('appointment/symptoms/', appointment_symptom_view, name="appointment_symptom"),
+    path('appointment/symptoms/delete/<slug:slug>/', appointment_symptom_delete_view, name="appointment_symptom_delete"),
+    path('appointment/symptoms/update/<slug:slug>/', appointment_symptom_update_view, name="appointment_symptom_update"),
+
+    path('appointment/', appointment_view, name="appointment"),
+    path('appointment/add', appointment_add_view, name="appointment_add"),
+    path('appointment/delete/<str:id>/', appointment_delete_view, name="appointment_delete"),
+
+
+    path('appointment/prescription/', appointment_prescription_view, name="appointment_prescription"),
+    path('appointment/prescription/add/', appointment_prescription_add_view, name="appointment_prescription_add"),
+    path('appointment/prescription/delete/<slug:slug>/', appointment_prescription_delete_view, name="appointment_prescription_delete"),
 ]
 
 
